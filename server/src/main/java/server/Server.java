880c825225384af9e3be7bd4151226de7eb70f1c
Jake McCoy <jhmccoy9@live.com>
2024-02-23 12:46:14 -0700

    starting to work on the clear service

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import spark.*;
+import com.google.gson.Gson;
+
 
 public class Server {
 
@@ -10,6 +12,9 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registerUser);
+        Spark.delete("/db", this::clearApp);
+
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -18,5 +23,32 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object registerUser(Request req, Response res)
+    {
+        /*
+        var pet = new Gson().fromJson(req.body(), Pet.class);
+        pet = service.addPet(pet);
+        webSocketHandler.makeNoise(pet.name(), pet.sound());
+        return new Gson().toJson(pet);
+         */
+        return new Object();
+    }
+
+    private Object clearApp(Request req, Response res)
+    {
+        /*
+        service.deleteAllPets();
+        res.status(204);
+        return "";
+         */
+
+
+        // right now, it's set up to return 200 no matter what...
+        res.status(200);
+        res.body("");
+
+        return "";
     }
 }
