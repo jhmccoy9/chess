4c8d3eab303cda19f057a043dfbc62b1850c3272
Jake McCoy <jhmccoy9@live.com>
2024-03-22 01:02:34 -0600

    main for starting client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import server.ServerFacade;
+import ui.PreloginUI;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client");
+
+        ServerFacade server = new ServerFacade("http://localhost:8080");
+        PreloginUI preloginUI = new PreloginUI(server);
+        preloginUI.run();
     }
 }
